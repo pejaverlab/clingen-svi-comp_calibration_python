17d78b4e85fb7fee4372a23663241e481d156bbe
Himanshu Sharma <himanshusharma.sh7@gmail.com>
2024-12-06 15:19:05 -0500

    Added infer feature

--- main.py
+++ main.py
@@ -36,52 +36,114 @@ def load_unlabelled_data(filepath):
 
 def getParser():
     parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(dest="command")
 
     parser.add_argument(
+        "infer",
+        action='store_true',
+    )
+    parser.add_argument(
+        "calibrate",
+        action='store_true',
+    )
+
+    parser_calibrate = subparsers.add_parser('calibrate')
+    parser_infer = subparsers.add_parser('infer')
+
+    parser_calibrate.add_argument(
         "--configfile",
         default=None,
         type=str,
         required=True,
     )
-    parser.add_argument(
-        "--tool",
-        default="Model",
-        type=str,
-        required=False,
-    )
-    parser.add_argument(
+
+    parser_calibrate.add_argument(
         "--outdir",
         default="out",
         type=str,
-        required=False,
+        required=True,
     )
-    parser.add_argument(
+    parser_calibrate.add_argument(
         "--labelled_data_file",
         default=None,
         type=str,
         required=True,
     )
-    parser.add_argument(
+    parser_calibrate.add_argument(
         "--unlabelled_data_file",
         default=None,
         type=str,
         required=False,
     )
-    parser.add_argument(
+    parser_calibrate.add_argument(
         "--reverse",
         action='store_true',
     )
 
+    parser_infer.add_argument(
+        "--score",
+        default=None,
+        type = float,
+        required=False,
+    )
+
+    parser_infer.add_argument(
+        "--score_file",
+        default=None,
+        type=str,
+        required=False,
+    )
+
+    parser_infer.add_argument(
+        "--calibrated_data_directory",
+        default=None,
+        type=str,
+        required=False
+    )
+    
     return parser
 
-def storeResults(outdir, tool, thresholds, posteriors_p, posteriors_b, all_pathogenic, all_benign, pthresh, bthresh, DiscountedThresholdP, DiscountedThresholdB, Post_p, Post_b):
 
-    fname = os.path.join(outdir,tool + "-pathogenic.txt")
+
+def infer_single(score, pthreshdiscounted, bthreshdiscounted):
+    if(not np.isnan(pthreshdiscounted[0]) and score > pthreshdiscounted[0]):
+        return 8
+    if(not np.isnan(pthreshdiscounted[1]) and score > pthreshdiscounted[1]):
+        return 4
+    if(not np.isnan(pthreshdiscounted[2]) and score > pthreshdiscounted[2]):
+        return 3
+    if(not np.isnan(pthreshdiscounted[3]) and score > pthreshdiscounted[3]):
+        return 2
+    if(not np.isnan(pthreshdiscounted[4]) and score > pthreshdiscounted[4]):
+        return 1
+    if(not np.isnan(bthreshdiscounted[0]) and score < bthreshdiscounted[0]):
+        return -8
+    if(not np.isnan(bthreshdiscounted[1]) and score < bthreshdiscounted[1]):
+        return -4
+    if(not np.isnan(bthreshdiscounted[2]) and score < bthreshdiscounted[2]):
+        return -3
+    if(not np.isnan(bthreshdiscounted[3]) and score < bthreshdiscounted[3]):
+        return -2
+    if(not np.isnan(bthreshdiscounted[4]) and score < bthreshdiscounted[4]):
+        return -1
+    else:
+        return 0
+
+
+def infer(scores, calibrated_data_directory):
+    pthreshdiscounted = np.loadtxt(os.path.join(calibrated_data_directory,"pthreshdiscounted.txt"))
+    bthreshdiscounted = np.loadtxt(os.path.join(calibrated_data_directory,"bthreshdiscounted.txt"))
+    return [infer_single(e,pthreshdiscounted, bthreshdiscounted) for e in scores]
+
+
+def storeResults(outdir, thresholds, posteriors_p, posteriors_b, all_pathogenic, all_benign, pthresh, bthresh, DiscountedThresholdP, DiscountedThresholdB, Post_p, Post_b):
+
+    fname = os.path.join(outdir, "pathogenic_posterior.txt")
     tosave = np.array([thresholds,posteriors_p]).T
-    np.savetxt(fname,tosave , delimiter='\t', fmt='%f')
-    fname = os.path.join(outdir,tool + "-benign.txt")
+    np.savetxt(fname,tosave , delimiter='\t', fmt='%f', header="score\tposterior")
+    fname = os.path.join(outdir,"benign_posterior.txt")
     tosave = np.array([np.flip(thresholds),posteriors_b]).T
-    np.savetxt(fname,tosave , delimiter='\t', fmt='%f')
+    np.savetxt(fname,tosave , delimiter='\t', fmt='%f', header="score\tposterior")
 
     import matplotlib.pyplot as plt
     fig, ax = plt.subplots()
@@ -90,7 +152,7 @@ def storeResults(outdir, tool, thresholds, posteriors_p, posteriors_b, all_patho
     ax.plot(np.flip(thresholds),benign95, color='gray', label = "One-sided confidence bound")
     ax.set_xlabel("score")
     ax.set_ylabel("posterior")
-    ax.set_title(tool)
+    ax.set_title("Posterior-Score Graph")
     ax.axhline(Post_b[4], linestyle='dotted', color='steelblue', label = "BP4_Supporting : " + str(round(Post_b[4],4)) )
     ax.axhline(Post_b[3], linestyle='dashed', color='steelblue', label = "BP4_Moderate : " + str(round(Post_b[3],4)) )
     ax.axhline(Post_b[2], linestyle='dashdot', color='steelblue', label = "BP4_Moderate+ : " + str(round(Post_b[2],4)) )
@@ -98,7 +160,7 @@ def storeResults(outdir, tool, thresholds, posteriors_p, posteriors_b, all_patho
     ax.axhline(Post_b[0], linestyle='solid', color='steelblue', label = "BP4_VeryStrong: " + str(round(Post_b[0],4)) ) 
     ax.set_ylim([0.975, 1.001])
     plt.legend(loc="lower left", fontsize="xx-small")
-    plt.savefig(os.path.join(outdir,tool+"-benign.png"))
+    plt.savefig(os.path.join(outdir,"benign.png"))
     ax.clear()
 
     ax.plot(thresholds,posteriors_p , linewidth=2.0, color='b')
@@ -106,33 +168,30 @@ def storeResults(outdir, tool, thresholds, posteriors_p, posteriors_b, all_patho
     ax.plot(thresholds,pathogenic95, color='gray', label = "One-sided confidence bound")
     ax.set_xlabel("score")
     ax.set_ylabel("posterior")
-    ax.set_title(tool)
+    ax.set_title("Posterior-Score Graph")
     ax.axhline(Post_p[4], linestyle='dotted', color='r' ,label = "PP3_Supporting : " + str(round(Post_p[4],4)))
     ax.axhline(Post_p[3], linestyle='dashed', color='r', label = "PP3_Moderate : " + str(round(Post_p[3],4)))
     ax.axhline(Post_p[2], linestyle='dashdot', color='r', label = "Moderate+ : " + str(round(Post_p[2],4)))
     ax.axhline(Post_p[1], linestyle=(5, (10, 3)), color='r', label = "PP3_Strong : " + str(round(Post_p[1],4)))
     ax.axhline(Post_p[0], linestyle='solid', color='r', label = "PP3_VeryStrong : " + str(round(Post_p[0],4)))
     plt.legend(loc="upper left", fontsize="xx-small")
-    plt.savefig(os.path.join(outdir, tool+"-pathogenic.png"))
+    plt.savefig(os.path.join(outdir, "pathogenic.png"))
 
-    fname = os.path.join(outdir,tool + "-pthresh.txt")
+    fname = os.path.join(outdir,"pthresh.txt")
     np.savetxt(fname, pthresh , delimiter='\t', fmt='%f')
-    fname = os.path.join(outdir,tool + "-bthresh.txt")
+    fname = os.path.join(outdir,"bthresh.txt")
     np.savetxt(fname, bthresh , delimiter='\t', fmt='%f')
 
-    fname = os.path.join(outdir,tool + "-pthreshdiscounted.txt")
+    fname = os.path.join(outdir,"pthreshdiscounted.txt")
+    stringtosave = "Supporting"
     np.savetxt(fname, DiscountedThresholdP , delimiter='\t', fmt='%f')
-    fname = os.path.join(outdir,tool + "-bthreshdiscounted.txt")
+    fname = os.path.join(outdir,"bthreshdiscounted.txt")
     np.savetxt(fname, DiscountedThresholdB , delimiter='\t', fmt='%f')
 
 
     
-
-def main():
-
-    parser = getParser()
-    args = parser.parse_args()
-    tool = args.tool
+def calibrate(args):
+        
     labeldatafile = args.labelled_data_file
     udatafile = args.unlabelled_data_file
     reverse = args.reverse
@@ -165,14 +224,8 @@ def main():
         alpha = configmodule.alpha
         c = get_tavtigian_c(alpha)
 
-    print(c)
-
-
-
     x,y = load_labelled_data(labeldatafile)
     g = load_unlabelled_data(udatafile)
-    print(g[:100])
-    #return
 
     x = np.array(x)
     y = np.array(y)
@@ -206,12 +259,32 @@ def main():
     DiscountedThresholdP = LocalCalibrateThresholdComputation.get_discounted_thresholds(pthresh, Post_p, B, discountonesided, 'pathogenic')
     DiscountedThresholdB = LocalCalibrateThresholdComputation.get_discounted_thresholds(bthresh, Post_b, B, discountonesided, 'benign')
 
-    storeResults(outdir, tool, thresholds, posteriors_p, posteriors_b, all_pathogenic[1:].T, all_benign[1:].T, pthresh, bthresh, DiscountedThresholdP, DiscountedThresholdB, Post_p, Post_b)
+    storeResults(outdir, thresholds, posteriors_p, posteriors_b, all_pathogenic[1:].T, all_benign[1:].T, pthresh, bthresh, DiscountedThresholdP, DiscountedThresholdB, Post_p, Post_b)
     
     print("Thresholds: ", pthresh)
     print("Discounted Thresholds: ", DiscountedThresholdP)
 
 
+    
+def main():
 
+    
+    parser = getParser()
+    args = parser.parse_args()
+    
+    if(args.command=="infer"):
+        if (args.score):
+            ans = infer([args.score], args.calibrated_data_directory)
+            print(ans[0])
+        elif (args.score_file):
+            scores = np.loadtxt(args.score_file)
+            ans = infer(scores, args.calibrated_data_directory)
+            print(ans)
+        return
+
+    if(args.command == "calibrate"):
+        calibrate(args)
+
+        
 if __name__ == '__main__':
     main()
